b4d9a24b543cef7dfcfe010774ffff8186251de8
JakeV64 <jakevonah@gmail.com>
2025-02-18 17:56:03 -0600

    app changes

--- portfolio-v/src/App.jsx
+++ portfolio-v/src/App.jsx
@@ -3,32 +3,95 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  return (
-    <>
-      <header>
-        <nav aria-label="Main Navigation">
-          <ul>
-            <li>Jacob Von Ah</li>
-            <li className="list">About Me</li>
-            <li className="list">Portfolio</li>
-            <li className="list">Contact</li>
-            <li className="list">Resume</li>
-          </ul>
-        </nav>
-      </header>
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-      <main>
-        <section className="about">
-          <h1>About Me</h1>
-          <p>
-            Hi, my name is Jacob Von Ah. I am from Illinois, currently a student
-            at The University of Wisconsin studying coding.
-          </p>
-        </section>
-      </main>
-    </>
-  )
+function Header() {
+  return (
+    <header>
+      <nav aria-label="Main Navigation">
+        <ul>
+          <li>Jacob Von Ah</li>
+          <li className="list">
+            <Link to="/">About Me</Link>
+          </li>
+          <li className="list">
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li className="list">
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li className="list">
+            <Link to="/resume">Resume</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default App
+function About() {
+  return (
+    <section className="about">
+      <h1>About Me</h1>
+      <p>
+        Hi, my name is Jacob Von Ah. I am from Illinois, currently a student at
+        The University of Wisconsin studying coding. The past few years I've
+        been working as a line cook, but I've always had a passion for
+        technology. I am excited to learn more about coding and web development.
+      </p>
+    </section>
+  );
+}
+
+function Portfolio() {
+  return (
+    <section className="portfolio">
+      <h1>Portfolio</h1>
+      <p>Here are some projects I've worked on.</p>
+      {/* Add your projects here */}
+    </section>
+  );
+}
+
+function Contact() {
+  return (
+    <section className="contact">
+      <h1>Contact</h1>
+      <p>Feel free to reach out to me through any of the methods below.</p>
+      {/* Add your contact info here */}
+    </section>
+  );
+}
+
+function Resume() {
+  return (
+    <section className="resume">
+      <h1>Resume</h1>
+      <p>Here is my resume with all the relevant details about my work experience and education.</p>
+      {/* Add resume info here */}
+    </section>
+  );
+}
+
+function Footer() {
+  return <footer><p>Created By Jacob Von Ah 2025</p></footer>;
+}
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
